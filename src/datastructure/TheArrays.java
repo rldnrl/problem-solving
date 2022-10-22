097eba37e703f28a55c6381ee9b91cb29b67164d
rldnrl <devpark1002@gmail.com>
2022-10-22 19:29:43 +0900

    feat: 배열

---
+++ src/datastructure/TheArrays.java
@@ -0,0 +1,41 @@
+package datastructure;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        colors[2] = "yellow";
+
+        System.out.println(Arrays.toString(colors));
+
+        int[] numbers = {100, 200};
+
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
+
+        for (String color: colors) {
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
+    }
+}
