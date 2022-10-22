39700daaa019f2571b6ec7a279775cb45752d233
rldnrl <devpark1002@gmail.com>
2022-10-22 19:49:05 +0900

    feat: List

---
+++ src/datastructure/TheList.java
@@ -0,0 +1,34 @@
+package datastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheList {
+    public static void main(String[] args) {
+        List<String> colorsImmutable = List.of(
+                "blue",
+                "yellow"
+        );
+        // colorsImmutable.add("blue"); // not working
+
+        List<String> colors = new ArrayList<>();
+        colors.add("blue");
+        colors.add("purple");
+        colors.add("yellow");
+
+        System.out.println(colors.size());
+        System.out.println(colors.contains("yellow"));
+        System.out.println(colors.contains("pink"));
+        System.out.println(colors);
+
+        for (String color: colors) {
+            System.out.println(color);
+        }
+
+        colors.forEach(System.out::println);
+
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
+}
