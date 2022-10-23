369122d085b9fc12b5787d1e878dfaddd1d0b8f9
rldnrl <devpark1002@gmail.com>
2022-10-23 22:46:48 +0900

    feat: main 제거

--- src/leetcode/RotateRightArray.java
+++ src/leetcode/RotateRightArray.java
@@ -3,12 +3,6 @@ package leetcode;
 import java.util.Arrays;
 
 public class RotateRightArray {
-    public static void main(String[] args) {
-        int[] test1 = new int[] {1,2,3,4,5,6,7};
-        rotateRight(test1, 3);
-        System.out.println(Arrays.toString(test1));
-    }
-
     public static void rotateRight(int[] nums, int k) {
         int size = nums.length;
         int[] copiedNums = Arrays.copyOf(nums, size);
