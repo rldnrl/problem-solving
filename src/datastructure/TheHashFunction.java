d594ec496aa40dbf722322e8155582e093204994
rldnrl <devpark1002@gmail.com>
2022-10-23 20:13:17 +0900

    feat: HashFunction HashCode

---
+++ src/datastructure/TheHashFunction.java
@@ -0,0 +1,45 @@
+package datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class TheHashFunction {
+    static class Person {
+        String name;
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+    record Diamond(String name) {}
+
+    public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+
+        map.put(new Person("Jamila"), new Diamond("African Diamond"));
+
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(map.get(new Person("Jamila")));
+    }
+}
