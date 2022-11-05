bba2712d5a36c59fb91b1fce95bc3c5627b7f172
rldnrl <devpark1002@gmail.com>
2022-11-05 17:02:03 +0900

    feat: 예시 삭제

--- src/algorithms/recursion/RobotGridMaze/Main.java
+++ src/algorithms/recursion/RobotGridMaze/Main.java
@@ -6,32 +6,31 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        /* R - 로봇, # - 미로, T - 도달 위치
-        (m, n) +           +
-               |R| | | | | |
-               |R|R| | | | |
-               |#|R|R|R|R| |
-               |#|#|#|#|R| |
-               | | | | |R| |
-               | | | | |R|T|
-               +           + (0, 0)
-        */
-
         // LinkedHashSet에 경로를 저장합니다.
-        Set<Point> path = new LinkedHashSet<>();
+        Set<Point> path1 = new LinkedHashSet<>();
 
         // 미로를 정의합니다.
-        boolean[][] maze  = new boolean[6][6];
-        maze[2][0]=true;
-        maze[3][0]=true;
-        maze[3][1]=true;
-        maze[3][2]=true;
-        maze[3][3]=true;
+        boolean[][] maze1  = new boolean[6][6];
+        maze1[2][0]=true;
+        maze1[3][0]=true;
+        maze1[3][1]=true;
+        maze1[3][2]=true;
+        maze1[3][3]=true;
 
         // 경로를 계산하고 표시합니다.
-        RobotGridMaze.computePath(5, 5, maze, path);
+        RobotGridMaze.computePath(5, 5, maze1, path1);
 
-        System.out.println("Computed path (plain recursion):");
-        path.forEach(System.out::println);
+        System.out.println("Computed path1 (plain recursion):");
+        path1.forEach(System.out::println);
+
+        Set<Point> path2 = new LinkedHashSet<>();
+
+        boolean[][] maze2  = new boolean[2][2];
+        maze2[0][1] = true;
+
+        RobotGridMaze.computePath(1, 1, maze2, path2);
+
+        System.out.println("Computed path2 (plain recursion):");
+        path2.forEach(System.out::println);
     }
 }
