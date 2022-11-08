be980545432e8f6de2c3d870e68faf92fc29c305
rldnrl <devpark1002@gmail.com>
2022-11-08 21:59:43 +0900

    test: write test code ValidParentheses

---
+++ test/leetcode/ValidParenthesesTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidParenthesesTest {
+    @Test
+    void validParenthesesTest() {
+        String s1 = "()";
+        Assertions.assertTrue(ValidParentheses.isValid(s1));
+
+        String s2 = "()[]{}";
+        Assertions.assertTrue(ValidParentheses.isValid(s2));
+
+        String s3 = "(]";
+        Assertions.assertFalse(ValidParentheses.isValid(s3));
+
+        String s4 = "}}}}}";
+        Assertions.assertFalse(ValidParentheses.isValid(s4));
+
+        String s5 = "{[{]]]]";
+        Assertions.assertFalse(ValidParentheses.isValid(s5));
+    }
+}
