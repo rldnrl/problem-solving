ed561d747d906edfe29df050cba2fe4a274bce14
Kiwook, Park <devpark1002@gmail.com>
2022-11-11 23:26:10 +0900

    Time: 6 ms (15.27%), Space: 42.1 MB (37.61%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> hashMap = new HashMap<>();
+
+        hashMap.put(')', '(');
+        hashMap.put('}', '{');
+        hashMap.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!hashMap.containsKey(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            } else if (stack.isEmpty() || !Objects.equals(hashMap.get(s.charAt(i)), stack.pop())) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
