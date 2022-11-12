8d772551688e674a7f5a66c2a37024b6a592e5ba
rldnrl <devpark1002@gmail.com>
2022-11-12 11:16:09 +0900

    chore: remove comment

--- src/algorithms/recursion/RobotGridAllPaths/Main.java
+++ src/algorithms/recursion/RobotGridAllPaths/Main.java
@@ -3,11 +3,11 @@ package algorithms.recursion.RobotGridAllPaths;
 public class Main {
     public static void main(String[] args) {
         // 6×6 격자
-//        int result1 = RobotGridAllPaths.countPaths(6, 6);
-//        int result2 = RobotGridAllPaths.countPathsBottomUp(6, 6);
-//
-//        System.out.println("Number of unique paths via plain recursion: " + result1);
-//        System.out.println("Number of unique paths via Bottom Up: " + result2);
+        int result1 = RobotGridAllPaths.countPaths(6, 6);
+        int result2 = RobotGridAllPaths.countPathsBottomUp(6, 6);
+
+        System.out.println("Number of unique paths via plain recursion: " + result1);
+        System.out.println("Number of unique paths via Bottom Up: " + result2);
 
         int result3 = RobotGridAllPaths.countPaths(2, 2);
         int result4 = RobotGridAllPaths.countPaths(3, 3);
