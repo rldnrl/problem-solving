00ca9c35a74768090d1f470def37b1e63c699fff
Kiwook, Park <devpark1002@gmail.com>
2022-11-14 21:49:03 +0900

    Time: 1277 ms (17.46%), Space: 47.2 MB (30.64%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static String removeNonCharacter(String s) {
+        return s.replaceAll("[^A-Za-z0-9]", "");
+    }
+    
+    public boolean isPalindrome(String s) {
+        String lowerString = s.toLowerCase();
+        String lowerStringRemovedNonCharacter = removeNonCharacter(lowerString);
+
+        int right = lowerStringRemovedNonCharacter.length() - 1;
+        int left = 0;
+
+        while (left < right) {
+            if (lowerStringRemovedNonCharacter.charAt(left) != lowerStringRemovedNonCharacter.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
