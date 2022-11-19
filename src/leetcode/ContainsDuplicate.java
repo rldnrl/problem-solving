c6ad26d240c47d671df4bd6502ba606d42615366
rldnrl <devpark1002@gmail.com>
2022-11-19 14:30:37 +0900

    feat: implement ContainsDuplicate using hash set

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
     public static boolean containsDuplicate(int[] nums) {
@@ -16,5 +17,18 @@ public class ContainsDuplicate {
         }
 
         return countOfNums.values().stream().anyMatch(x -> x > 1);
+    }
+
+    public static boolean containsDuplicateWithHashSet(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int num: nums) {
+            if (hashSet.contains(num)) {
+                return true;
+            }
+            hashSet.add(num);
+        }
+
+        return false;
     }
 }
