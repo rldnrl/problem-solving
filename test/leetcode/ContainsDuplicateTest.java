dbdee453baea3a0640f8663092fe324f221da5e8
rldnrl <devpark1002@gmail.com>
2022-11-19 13:56:05 +0900

    test: write test code about ContainsDuplicate

---
+++ test/leetcode/ContainsDuplicateTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class ContainsDuplicateTest {
+    @Test
+    void containDuplicate() {
+        int[] nums1 = new int[]{1,2,3,1};
+        Assertions.assertTrue(ContainsDuplicate.containsDuplicate(nums1));
+
+        int[] nums2 = new int[]{1,2,3};
+        Assertions.assertFalse(ContainsDuplicate.containsDuplicate(nums2));
+
+        int[] nums3 = new int[]{1,1,1,3,3,4,3,2,4,2};
+        Assertions.assertTrue(ContainsDuplicate.containsDuplicate(nums3));
+    }
+}
