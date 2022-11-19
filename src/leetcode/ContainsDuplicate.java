f07c9957a3c66f020e5e687d875381b75536cdde
rldnrl <devpark1002@gmail.com>
2022-11-19 13:56:33 +0900

    feat: implement ContainsDuplicate

---
+++ src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> countOfNums = new HashMap<>();
+
+        for (int num : nums) {
+            if (!countOfNums.containsKey(num)) {
+                countOfNums.put(num, 1);
+            } else {
+                int count = countOfNums.get(num);
+                countOfNums.put(num, count + 1);
+            }
+        }
+
+        return countOfNums.values().stream().anyMatch(x -> x > 1);
+    }
+}
