519dbc86828d38d93bfd755c3bd35568f64859d6
rldnrl <devpark1002@gmail.com>
2022-11-19 14:30:49 +0900

    test: write test ContainsDuplicate using hash set

--- test/leetcode/ContainsDuplicateTest.java
+++ test/leetcode/ContainsDuplicateTest.java
@@ -15,4 +15,16 @@ class ContainsDuplicateTest {
         int[] nums3 = new int[]{1,1,1,3,3,4,3,2,4,2};
         Assertions.assertTrue(ContainsDuplicate.containsDuplicate(nums3));
     }
+
+    @Test
+    void containDuplicateWithHashSet() {
+        int[] nums1 = new int[]{1,2,3,1};
+        Assertions.assertTrue(ContainsDuplicate.containsDuplicateWithHashSet(nums1));
+
+        int[] nums2 = new int[]{1,2,3};
+        Assertions.assertFalse(ContainsDuplicate.containsDuplicateWithHashSet(nums2));
+
+        int[] nums3 = new int[]{1,1,1,3,3,4,3,2,4,2};
+        Assertions.assertTrue(ContainsDuplicate.containsDuplicateWithHashSet(nums3));
+    }
 }
