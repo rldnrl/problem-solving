dafa42501ca0a63fe6143966130bf5620a1b9814
Kiwook, Park <devpark1002@gmail.com>
2022-11-21 13:32:25 +0900

    Time: 33 ms (59.06%), Space: 42.6 MB (93.92%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,21 +3,15 @@ class Solution {
         HashMap<Character, Integer> chars = new HashMap<>();
         
         for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (!chars.containsKey(currentChar)) {
-                chars.put(currentChar, i);
-            } else {
-                chars.put(currentChar, -1);
+            chars.put(s.charAt(i), chars.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (chars.get(s.charAt(i)) == 1) {
+                return i;
             }
         }
         
-        int min = Integer.MAX_VALUE;
-        for (Map.Entry<Character, Integer> entry : chars.entrySet()) {
-            if (entry.getValue() > -1 && entry.getValue() < min) {
-                min = entry.getValue();
-            }
-        }
-        
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return -1;
     }
 }
