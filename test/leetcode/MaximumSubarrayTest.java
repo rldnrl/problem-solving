4100f7471abc4bdee87b780e329605343564e4a1
rldnrl <devpark1002@gmail.com>
2022-11-26 16:45:47 +0900

    test: write test MaximumSubarray using sliding window

--- test/leetcode/MaximumSubarrayTest.java
+++ test/leetcode/MaximumSubarrayTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class MaximumSubarrayTest {
     @Test
-    void maximumSubarrayTest() {
+    void maximumSubarrayWithBruteforceTest() {
         int[] nums1 = new int[]{-2,1,-3,4,-1,2,1,-5,4};
         int result1 = 6;
         Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforce(nums1), result1);
@@ -17,5 +17,20 @@ class MaximumSubarrayTest {
         int[] nums3 = new int[]{5,4,-1,7,8};
         int result3 = 23;
         Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforce(nums3), result3);
+    }
+
+    @Test
+    void maximumSubarrayWithSlidingWindow() {
+        int[] nums1 = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int result1 = 6;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithSlidingWindow(nums1), result1);
+
+        int[] nums2 = new int[]{1};
+        int result2 = 1;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithSlidingWindow(nums2), result2);
+
+        int[] nums3 = new int[]{5,4,-1,7,8};
+        int result3 = 23;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithSlidingWindow(nums3), result3);
     }
 }
