8341a2df068e329895b1e2ee1fbe30230b5d666f
rldnrl <devpark1002@gmail.com>
2022-11-26 16:45:33 +0900

    feat: implement MaximumSubarray using sliding window

--- src/leetcode/MaximumSubarray.java
+++ src/leetcode/MaximumSubarray.java
@@ -2,9 +2,13 @@ package leetcode;
 
 /**
  * <a href="https://leetcode.com/problems/maximum-subarray/">Maximum Subarray</a>
- * Time Complex: O(n^3)
  */
 public class MaximumSubarray {
+    /**
+     * Time Complex: O(n^3)
+     * @param nums
+     * @return
+     */
     public static int maxSubarrayWithBruteforce(int[] nums) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
@@ -18,5 +22,23 @@ public class MaximumSubarray {
         }
 
         return max;
+    }
+
+    /**
+     * Time Complex: O(n)
+     */
+    public static int maxSubarrayWithSlidingWindow(int[] nums) {
+        int maxSub = nums[0];
+        int curSum = 0;
+
+        for (int element: nums) {
+            if (curSum < 0) {
+                curSum = 0;
+            }
+            curSum += element;
+            maxSub = Math.max(maxSub, curSum);
+        }
+
+        return maxSub;
     }
 }
