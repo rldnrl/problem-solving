1cba0d9f1e8861485ae8969c63e8171e62c47428
rldnrl <devpark1002@gmail.com>
2022-11-26 17:16:31 +0900

    test: write test code about maxSubarrayWithBruteforceOptimized

--- test/leetcode/MaximumSubarrayTest.java
+++ test/leetcode/MaximumSubarrayTest.java
@@ -20,6 +20,25 @@ class MaximumSubarrayTest {
     }
 
     @Test
+    void maximumSubarrayWithBruteforceOptimized() {
+        int[] nums1 = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int result1 = 6;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforceOptimized(nums1), result1);
+
+        int[] nums2 = new int[]{1};
+        int result2 = 1;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforceOptimized(nums2), result2);
+
+        int[] nums3 = new int[]{5,4,-1,7,8};
+        int result3 = 23;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforceOptimized(nums3), result3);
+
+        int[] nums4 = new int[]{-5, -4,-1, -7, -8};
+        int result4 = -1;
+        Assertions.assertEquals(MaximumSubarray.maxSubarrayWithBruteforceOptimized(nums4), result4);
+    }
+
+    @Test
     void maximumSubarrayWithSlidingWindow() {
         int[] nums1 = new int[]{-2,1,-3,4,-1,2,1,-5,4};
         int result1 = 6;
