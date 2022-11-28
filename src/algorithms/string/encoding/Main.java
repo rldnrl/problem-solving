b0b1ac36e3a45bf206d50fac1ec5a3b5ab9353eb
rldnrl <devpark1002@gmail.com>
2022-11-28 20:52:25 +0900

    test: write test encode white space with replace

--- src/algorithms/string/encoding/Main.java
+++ src/algorithms/string/encoding/Main.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         char[] str = "  String   with spaces  ".toCharArray();
-
         System.out.println("Before: " + Arrays.toString(str));
 
         char[] result = EncodingString.encodeWhitespaces(str);
+        System.out.println("After1: " + Arrays.toString(result));
 
-        System.out.println("After: " + Arrays.toString(result));
+        char[] result2 = EncodingString.encodeWhitespacesWithReplace(str);
+        System.out.println("After2: " + Arrays.toString(result2));
     }
 }
