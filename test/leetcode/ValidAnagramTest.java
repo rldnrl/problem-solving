555eba021885258421cebbde8d70c1fae594417d
rldnrl <devpark1002@gmail.com>
2022-12-07 20:49:22 +0900

    test: write test code about valid anagram

--- test/leetcode/ValidAnagramTest.java
+++ test/leetcode/ValidAnagramTest.java
@@ -1,10 +1,22 @@
 package leetcode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ValidAnagramTest {
+class ValidAnagramTest {
     @Test
     void isAnagramTest() {
+        String s1 = "anagram";
+        String t1 = "nagaram";
 
+        Assertions.assertTrue(ValidAnagram.isAnagramWithHashMap(s1, t1));
+        Assertions.assertTrue(ValidAnagram.isAnagramWithSort(s1, t1));
+        Assertions.assertTrue(ValidAnagram.isAnagramWithFrequency(s1, t1));
+
+        String s2 = "cat";
+        String t2 = "rat";
+        Assertions.assertFalse(ValidAnagram.isAnagramWithHashMap(s2, t2));
+        Assertions.assertFalse(ValidAnagram.isAnagramWithSort(s2, t2));
+        Assertions.assertFalse(ValidAnagram.isAnagramWithFrequency(s2, t2));
     }
 }
