803e5305ecccfe3ace9b204f6714a6ca534a9c3a
rldnrl <devpark1002@gmail.com>
2022-12-07 20:34:38 +0900

    feat: implement ValidAnagram with sort

--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -37,4 +37,14 @@ public class ValidAnagram {
 
         return true;
     }
+
+    public static boolean isValidWithSort(String s, String t) {
+        char[] arrayOfS = s.toCharArray();
+        char[] arrayOfT = t.toCharArray();
+
+        Arrays.sort(arrayOfS);
+        Arrays.sort(arrayOfT);
+
+        return Arrays.equals(arrayOfS, arrayOfT);
+    }
 }
