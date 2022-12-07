bbdd7271db478596c4d731fe98d252cc78412d5e
rldnrl <devpark1002@gmail.com>
2022-12-07 20:34:03 +0900

    feat: implement ValidAnagram with HashMap

--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -1,9 +1,39 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class ValidAnagram {
-    public static boolean isAnagram(String s, String t) {
-        char[] charArrayS = s.toCharArray();
-        char[] charArrayT = s.toCharArray();
+    public static boolean isAnagramWithHashMap(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> countOfS = new HashMap<>();
+        Map<Character, Integer> countOfT = new HashMap<>();
+
+        for (char c: s.toCharArray()) {
+            countOfS.put(c, countOfS.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c: t.toCharArray()) {
+            countOfT.put(c, countOfT.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.print(countOfS);
+        System.out.print(countOfT);
+
+        for (Map.Entry<Character, Integer> entry: countOfS.entrySet()) {
+            char c = entry.getKey();
+            int count = entry.getValue();
+            if (!countOfT.containsKey(c)) {
+                return false;
+            }
+
+            if (!Objects.equals(count, countOfT.get(c))) {
+                return false;
+            }
+        }
 
         return true;
     }
