407ca1290a22ba6e7d9e6d9b70593bfcc6826f31
rldnrl <devpark1002@gmail.com>
2022-12-09 18:59:48 +0900

    feat: implement group anagram with hashmap

---
+++ src/leetcode/GroupAnagram.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.*;
+
+public class GroupAnagram {
+    public static String sortString(String s) {
+        String[] arrayOfS = s.split("");
+        Arrays.sort(arrayOfS);
+        return String.join("", arrayOfS);
+    }
+
+    public static List<List<String>> groupAnagramsWithHashMap(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s: strs) {
+            String sortedS = sortString(s);
+            if (map.containsKey(sortedS)) {
+                map.get(sortedS).add(s);
+            } else {
+                List<String> newValue = new ArrayList<>();
+                newValue.add(s);
+                map.put(sortedS, newValue);
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry: map.entrySet()) {
+            result.add(entry.getValue());
+        }
+
+        return result;
+    }
+}
