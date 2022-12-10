8e5a6492c8c27870064b8c680ceb1e792445b161
rldnrl <devpark1002@gmail.com>
2022-12-10 18:32:24 +0900

    feat: implement GroupAnagram with frequency

--- src/leetcode/GroupAnagram.java
+++ src/leetcode/GroupAnagram.java
@@ -25,4 +25,20 @@ public class GroupAnagram {
 
         return result;
     }
+
+    public static List<List<String>> groupAnagramsWithFrequency(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str: strs) {
+            char[] count = new char[26];
+            for (char c: str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = new String(count);
+            map.computeIfAbsent(key, k -> new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
 }
