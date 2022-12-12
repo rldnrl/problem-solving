c977814688cc34b05477dcb069809b1507ca9170
rldnrl <devpark1002@gmail.com>
2022-12-12 22:44:15 +0900

    feat: 기사단원의 무기 최적화

--- src/programmers/lv1/WeaponOfTemplar.java
+++ src/programmers/lv1/WeaponOfTemplar.java
@@ -1,8 +1,13 @@
 package programmers.lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class WeaponOfTemplar {
     public static int solutionWithBruteforce(int number, int limit, int power) {
-        int[] divisions = new int[number];
+        int[] arrayOfCount = new int[number];
         for (int n = 1; n <= number; n++) {
             int countOfDivision = 0;
             for (int i = 1; i <= n; i++) {
@@ -10,15 +15,43 @@ public class WeaponOfTemplar {
                     countOfDivision++;
                 }
             }
-            divisions[n - 1] = countOfDivision;
+            arrayOfCount[n - 1] = countOfDivision;
         }
 
         int answer = 0;
         for (int i = 0; i < number; i++) {
-            if (divisions[i] > limit) {
+            if (arrayOfCount[i] > limit) {
                 answer += power;
             } else {
-                answer += divisions[i];
+                answer += arrayOfCount[i];
+            }
+        }
+
+        return answer;
+    }
+
+    public static int solutionWithOptimization(int number, int limit, int power) {
+        int[] arrayOfCount = new int[number];
+
+        for (int n = 1; n <= number; n++) {
+            int countOfDivision = 0;
+            for (int i = 1; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    countOfDivision++;
+                    if (i != n / i) {
+                        countOfDivision++;
+                    }
+                }
+            }
+            arrayOfCount[n - 1] = countOfDivision;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < number; i++) {
+            if (arrayOfCount[i] > limit) {
+                answer += power;
+            } else {
+                answer += arrayOfCount[i];
             }
         }
 
@@ -30,10 +63,12 @@ public class WeaponOfTemplar {
         int limit1 = 3;
         int power1 = 2;
         System.out.println(solutionWithBruteforce(number1, limit1, power1));
+        System.out.println(solutionWithOptimization(number1, limit1, power1));
 
         int number2 = 10;
         int limit2 = 3;
         int power2 = 2;
         System.out.println(solutionWithBruteforce(number2, limit2, power2));
+        System.out.println(solutionWithOptimization(number2, limit2, power2));
     }
 }
