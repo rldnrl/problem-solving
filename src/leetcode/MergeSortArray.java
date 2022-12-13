573472b1449aec45591bf77927f537f35e6d4869
rldnrl <devpark1002@gmail.com>
2022-12-13 21:46:20 +0900

    feat: merge sorted array with two pointer

--- src/leetcode/MergeSortArray.java
+++ src/leetcode/MergeSortArray.java
@@ -14,4 +14,25 @@ public class MergeSortArray {
 
         Arrays.sort(nums1);
     }
+
+    public void mergeWithTwoPointer(int[] nums1, int m, int[] nums2, int n) {
+        int lastIndex = m + n - 1;
+
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[lastIndex] = nums1[m - 1];
+                m--;
+            } else {
+                nums1[lastIndex] = nums2[n - 1];
+                n--;
+            }
+            lastIndex--;
+        }
+
+        while (n > 0) {
+            nums1[lastIndex] = nums2[n - 1];
+            n--;
+            lastIndex--;
+        }
+    }
 }
