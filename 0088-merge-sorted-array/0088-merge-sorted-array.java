4f5d2ccd6a7f2ecff3b58e00e5c10b8ecb1f8054
Kiwook, Park <devpark1002@gmail.com>
2022-12-13 21:09:07 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (82.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,12 +3,12 @@ class Solution {
         int lastIndex = m + n - 1;
         
         while (m > 0 && n > 0) {
-            if (nums1[m - 1] < nums2[n - 1]) {
-                nums1[lastIndex] = nums2[n - 1];
-                n--;
-            } else {
+            if (nums1[m - 1] > nums2[n - 1]) {
                 nums1[lastIndex] = nums1[m - 1];
                 m--;
+            } else {
+                nums1[lastIndex] = nums2[n - 1];
+                n--;
             }
             lastIndex--;
         }
