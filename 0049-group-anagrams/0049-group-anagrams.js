2575c923b84383299818d2bcaf9ebe46334746ac
Kiwook, Park <devpark1002@gmail.com>
2022-12-17 18:19:00 +0900

    Time: 155 ms (80.59%), Space: 55 MB (19.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,18 +3,18 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const map = new Map()
-    strs.forEach((str) => {
-        const frequency = Array.from({ length: 26 }).fill(0);
-        for (const c of str) {
-          frequency[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    const map = strs.reduce((prev, cur) => {
+        const frequency = Array.from({ length: 26 }).fill(0)
+        for (const c of cur) {
+            frequency[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
         }
-
-        const key = frequency.map(count => `#${count}`).join("")
-        const values = map.get(key) || []
-        values.push(str)
-        map.set(key, values)
-    })
+        const key = frequency.map((count) => `#${count}`).join("")
+        const values = prev.get(key) || []
+        values.push(cur)
+        prev.set(key, values)
+        
+        return prev
+    }, new Map())
 
     return [...map.values()]
 };
