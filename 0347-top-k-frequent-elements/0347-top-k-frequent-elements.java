791244c7380f6a5678baa8c536bc51ebc87a937a
Kiwook, Park <devpark1002@gmail.com>
2022-12-18 22:51:26 +0900

    Time: 37 ms (36.45%), Space: 50.5 MB (50.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] answer = new int[k];
-        Map<Integer, Integer> map = new HashMap<>();
+Map<Integer, List<Integer>> bucket = new HashMap<>();
+        Map<Integer, Integer> frequency = new HashMap<>();
 
-        for (int num: nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for (int i = 0; i < nums.length + 1; i++) {
+            bucket.computeIfAbsent(i, key -> new ArrayList<>());
         }
 
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+        for (int num: nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
 
-        entryList.sort((a, b) -> b.getValue() - a.getValue());
-
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry: entryList) {
-            if (i == k) {
-                return answer;
+        for (int num: nums) {
+            int count = frequency.get(num);
+            if (!bucket.get(count).contains(num)) {
+                bucket.get(count).add(num);
             }
-            answer[i] = entry.getKey();
-            i++;
+        }
+
+        int index = 0;
+        int[] answer = new int[k];
+        for (int count = nums.length; count >= 0; count--) {
+            for (int element: bucket.get(count)) {
+                if (index != k) {
+                    answer[index] = element;
+                    index++;
+                }
+            }
         }
 
         return answer;
