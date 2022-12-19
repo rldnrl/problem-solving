6768276041865e82033ec889c6bb9dba497df195
rldnrl <devpark1002@gmail.com>
2022-12-19 14:01:36 +0900

    feat: implement top k frequent elements with bucket sort

--- src/leetcode/TopKFrequencyElements.java
+++ src/leetcode/TopKFrequencyElements.java
@@ -26,4 +26,51 @@ public class TopKFrequencyElements {
 
         return answer;
     }
+
+    public static int[] topKFrequentWithBucketSort(int[] nums, int k) {
+        Map<Integer, List<Integer>> bucket = new HashMap<>();
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            bucket.computeIfAbsent(i, key -> new ArrayList<>());
+        }
+
+        for (int num: nums) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num: nums) {
+            int count = frequency.get(num);
+            if (!bucket.get(count).contains(num)) {
+                bucket.get(count).add(num);
+            }
+        }
+
+        int index = 0;
+        int[] answer = new int[k];
+        for (int count = nums.length; count >= 0; count--) {
+            for (int element: bucket.get(count)) {
+                if (index != k) {
+                    answer[index] = element;
+                    index++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,1,1,2,2,3};
+        System.out.println(Arrays.toString(topKFrequentWithHashMap(nums1, 2)));
+        System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums1, 2)));
+
+        int[] nums2 = new int[]{1};
+        System.out.println(Arrays.toString(topKFrequentWithHashMap(nums2, 1)));
+        System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums2, 1)));
+
+        int[] nums3 = new int[]{4,1,-1,2,-1,2,3};
+        System.out.println(Arrays.toString(topKFrequentWithHashMap(nums3, 2)));
+        System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums3, 2)));
+    }
 }
