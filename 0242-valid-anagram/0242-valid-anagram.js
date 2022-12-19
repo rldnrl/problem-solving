7603d4c597e01f413c9d1cc4bb8b72ddde073028
Kiwook, Park <devpark1002@gmail.com>
2022-12-19 16:21:40 +0900

    Time: 88 ms (89.20%), Space: 42.8 MB (85.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,10 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-
-    const sortedS = s.split("").sort().join("")
-    const sortedT = t.split("").sort().join("")
-
-    return sortedS === sortedT
+    if (s.length !== t.length) return false
+    
+    const frequency = Array.from({ length: 26 }).fill(0)
+    
+    for (let i = 0; i < s.length; i++) {
+        const charOfS = s[i]
+        const charOfT = t[i]
+        
+        frequency[charOfS.charCodeAt(0) - 'a'.charCodeAt(0)]++
+        frequency[charOfT.charCodeAt(0) - 'a'.charCodeAt(0)]--
+    }
+    
+    for (const f of frequency) {
+        if (f !== 0) {
+            return false
+        }
+    }
+    
+    return true
 };
