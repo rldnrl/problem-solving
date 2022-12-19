863779cc39b9beaee6c3c0aa2bc3e560453b7f9f
Kiwook, Park <devpark1002@gmail.com>
2022-12-19 14:54:18 +0900

    Time: 41 ms (26.83%), Space: 56.8 MB (5.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,37 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
         Map<Integer, List<Integer>> bucket = new HashMap<>();
-        Map<Integer, Integer> frequency = new HashMap<>();
-
-        for (int i = 0; i < nums.length + 1; i++) {
-            bucket.computeIfAbsent(i, key -> new ArrayList<>());
-        }
-
+        
+        int size = nums.length;
+        
         for (int num: nums) {
-            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-
-        for (int num: nums) {
-            int count = frequency.get(num);
-            if (!bucket.get(count).contains(num)) {
-                bucket.get(count).add(num);
-            }
+        
+        for (int i = 0; i < size + 1; i++) {
+            bucket.computeIfAbsent(i, key -> new ArrayList());
         }
-
-        int index = 0;
+        
+        for (Map.Entry<Integer, Integer> freqEntry: freq.entrySet()) {
+            int num = freqEntry.getKey();
+            int count = freqEntry.getValue();
+            
+            bucket.computeIfAbsent(count, key -> new ArrayList<>());
+            bucket.get(count).add(num);
+        }
+        
+        int indexOfK = 0;
         int[] answer = new int[k];
-        for (int count = nums.length; count >= 0; count--) {
-            for (int element: bucket.get(count)) {
-                if (index != k) {
-                    answer[index] = element;
-                    index++;
+        for (int count = size; count >= 0; count--) {
+            for (int num: bucket.get(count)) {
+                answer[indexOfK++] = num;
+                if (indexOfK == k) {
+                    return answer;
                 }
             }
         }
-
+        
         return answer;
     }
 }
