283707668b73f97381d16191b331ad35c8b089cd
rldnrl <devpark1002@gmail.com>
2022-12-19 14:01:19 +0900

    feat: implement top k frequent elements with sort

---
+++ src/leetcode/TopKFrequencyElements.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.*;
+
+public class TopKFrequencyElements {
+    public static int[] topKFrequentWithHashMap(int[] nums, int k) {
+        int[] answer = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+
+        entryList.sort((a, b) -> b.getValue() - a.getValue());
+
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry: entryList) {
+            if (i == k) {
+                return answer;
+            }
+            answer[i] = entry.getKey();
+            i++;
+        }
+
+        return answer;
+    }
+}
