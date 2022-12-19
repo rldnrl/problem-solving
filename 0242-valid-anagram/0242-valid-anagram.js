2ef319180e5c20885c73a95961957d26c12c14ad
Kiwook, Park <devpark1002@gmail.com>
2022-12-19 16:11:25 +0900

    Time: 108 ms (76.02%), Space: 49.6 MB (7.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const sortedS = s.split("").sort().join("")
+    const sortedT = t.split("").sort().join("")
+
+    return sortedS === sortedT
+};
