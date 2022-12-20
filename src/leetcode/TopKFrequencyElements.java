ac4c966b715e345b037c98167e857a304553021c
rldnrl <devpark1002@gmail.com>
2022-12-20 21:08:52 +0900

    feat: implement top k frequent elements using heap

--- src/leetcode/TopKFrequencyElements.java
+++ src/leetcode/TopKFrequencyElements.java
@@ -66,17 +66,44 @@ public class TopKFrequencyElements {
         return answer;
     }
 
+    public static int[] topKFrequentElementsWithHeap(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int num: nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>(Comparator.comparingInt(count::get));
+
+        for (int num: count.keySet()) {
+            heap.add(num);
+            if (heap.size() > k) heap.poll();
+        }
+
+        int[] answer = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            if (!heap.isEmpty()) {
+                answer[i] = heap.poll();
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = new int[]{1,1,1,2,2,3};
         System.out.println(Arrays.toString(topKFrequentWithHashMap(nums1, 2)));
         System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums1, 2)));
+        System.out.println(Arrays.toString(topKFrequentElementsWithHeap(nums1, 2)));
 
         int[] nums2 = new int[]{1};
         System.out.println(Arrays.toString(topKFrequentWithHashMap(nums2, 1)));
         System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums2, 1)));
+        System.out.println(Arrays.toString(topKFrequentElementsWithHeap(nums2, 1)));
 
         int[] nums3 = new int[]{4,1,-1,2,-1,2,3};
         System.out.println(Arrays.toString(topKFrequentWithHashMap(nums3, 2)));
         System.out.println(Arrays.toString(topKFrequentWithBucketSort(nums3, 2)));
+        System.out.println(Arrays.toString(topKFrequentElementsWithHeap(nums3, 2)));
     }
 }
