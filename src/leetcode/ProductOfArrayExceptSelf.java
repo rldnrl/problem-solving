5d43b3f3efe9dbcffb65082cf038eafaece43f80
rldnrl <devpark1002@gmail.com>
2022-12-22 16:47:37 +0900

    feat: implement product of array except self with prefix and postfix

--- src/leetcode/ProductOfArrayExceptSelf.java
+++ src/leetcode/ProductOfArrayExceptSelf.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
     /**
      * Time Complexity: O(n^2)
@@ -18,5 +20,52 @@ public class ProductOfArrayExceptSelf {
         }
 
         return answer;
+    }
+
+
+    /**
+     * Time Complexity: O(n);
+     * Space Complexity: O(n)
+     */
+    public static int[] productExceptSelfWithPrefixPostfix1(int[] nums) {
+        int size = nums.length;
+        int[] prefixArray = new int[size];
+        int[] postfixArray = new int[size];
+
+
+        prefixArray[0] = nums[0];
+        for (int i = 1; i < size; i++) {
+            prefixArray[i] = prefixArray[i - 1] * nums[i];
+        }
+
+        postfixArray[size - 1] = nums[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            postfixArray[i] = postfixArray[i + 1] * nums[i];
+        }
+
+        int[] answer = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i == 0) {
+                answer[i] = postfixArray[i + 1];
+            } else if (i == size - 1) {
+                answer[i] = prefixArray[i - 1];
+            } else {
+                answer[i] = prefixArray[i - 1] * postfixArray[i + 1];
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,2,3,4};
+        int[] answer1 = new int[]{24,12,8,6};
+        System.out.println(Arrays.equals(productExceptSelfWithBruteforce(nums1), answer1));
+        System.out.println(Arrays.equals(productExceptSelfWithPrefixPostfix1(nums1), answer1));
+
+        int[] nums2 = new int[]{-1,1,0,-3,3};
+        int[] answer2 = new int[]{0,0,9,0,0};
+        System.out.println(Arrays.equals(productExceptSelfWithBruteforce(nums2), answer2));
+        System.out.println(Arrays.equals(productExceptSelfWithPrefixPostfix1(nums2), answer2));
     }
 }
