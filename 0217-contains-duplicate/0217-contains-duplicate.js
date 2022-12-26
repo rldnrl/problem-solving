32612a9b730d174e08b12ed4ae84467065002d3f
Kiwook, Park <devpark1002@gmail.com>
2022-12-26 15:37:23 +0900

    Time: 83 ms (95.97%), Space: 51.2 MB (32.62%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set();
+    
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+    
+    return false;
+};
