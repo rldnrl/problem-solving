da7414002acd5ffb4f2506a89ee6039693372f2f
Kiwook, Park <devpark1002@gmail.com>
2022-12-26 18:09:29 +0900

    Time: 6 ms (93.27%), Space: 42.9 MB (75.22%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> m1, Map.Entry<String, Integer> m2) {
+            if (m2.getValue() > m1.getValue()) {
+                return 1;
+            } else if (m2.getValue() < m1.getValue()) {
+                return -1;
+            } else {
+                return m1.getKey().compareToIgnoreCase(m2.getKey());
+            }
+        }
+    }
+    
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> freq = new HashMap<>();
+        
+        for (String word: words) {
+            freq.put(word, freq.getOrDefault(word, 0) + 1);
+        }
+        
+        List<Map.Entry<String, Integer>> freqEntryList = new ArrayList<>(freq.entrySet());
+        
+        freqEntryList.sort(new MyComparator());
+        
+        int indexOfAnswer = 0;
+        List<String> answer = new ArrayList<>();
+        for (Map.Entry<String, Integer> freqEntry: freqEntryList) {
+            answer.add(freqEntry.getKey());
+            indexOfAnswer++;
+            if (indexOfAnswer == k) {
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
