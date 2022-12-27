4d59f6f54edce46d0e5f112ffe123984fa7163b8
rldnrl <devpark1002@gmail.com>
2022-12-27 13:53:12 +0900

    feat: implement top k frequent words with bucket sort

--- src/leetcode/TopKFrequentWords.java
+++ src/leetcode/TopKFrequentWords.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.util.*;
 
 public class TopKFrequentWords {
-    public static class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+    public static class MyComparatorByMap implements Comparator<Map.Entry<String, Integer>> {
         @Override
         public int compare(Map.Entry<String, Integer> m1, Map.Entry<String, Integer> m2) {
             if (m1.getValue() < m2.getValue()) {
@@ -25,7 +25,7 @@ public class TopKFrequentWords {
 
         List<Map.Entry<String, Integer>> freqEntryList = new ArrayList<>(freq.entrySet());
 
-        freqEntryList.sort(new MyComparator());
+        freqEntryList.sort(new MyComparatorByMap());
 
         int indexOfAnswer = 0;
         List<String> answer = new ArrayList<>();
@@ -40,11 +40,46 @@ public class TopKFrequentWords {
         return answer;
     }
 
+    public static List<String> topKFrequentWordsWithBucketSort(String[] words, int k) {
+        Map<String, Integer> freq = new HashMap<>();
+        for (String word: words) {
+            freq.put(word, freq.getOrDefault(word, 0) + 1);
+        }
+
+        Map<Integer, List<String>> bucket = new HashMap<>();
+        for (int i = 0; i < words.length + 1; i++) {
+            bucket.computeIfAbsent(i, key -> new ArrayList<>());
+        }
+
+        for (Map.Entry<String, Integer> freqEntry: freq.entrySet()) {
+            int count = freqEntry.getValue();
+            String word = freqEntry.getKey();
+            bucket.get(count).add(word);
+        }
+
+        int indexOfAnswer = 0;
+        List<String> answer = new ArrayList<>();
+        for (int i = words.length; i >= 0; i--) {
+            bucket.get(i).sort(null);
+            for (String word: bucket.get(i)) {
+                answer.add(word);
+                indexOfAnswer++;
+                if (indexOfAnswer == k) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         String[] words1 = new String[]{"i","love","leetcode","i","love","coding"};
         System.out.println(topKFrequentWithHashMap(words1, 2));
+        System.out.println(topKFrequentWordsWithBucketSort(words1, 2));
 
         String[] words2 = new String[]{"the","day","is","sunny","the","the","the","sunny","is","is"};
         System.out.println(topKFrequentWithHashMap(words2, 4));
+        System.out.println(topKFrequentWordsWithBucketSort(words2, 4));
     }
 }
