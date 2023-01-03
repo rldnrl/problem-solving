06b392f52aef6d0ddb4f252b91fe74d98bffa83a
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-03 20:50:42 +0900

    Time: 122 ms (47.92%), Space: 49.5 MB (62.28%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let answer = 0;
+    
+    let left = 0;
+    let right = height.length - 1;
+    while (left < right) {
+        const area = (right - left) * Math.min(height[left], height[right]);
+        answer = Math.max(answer, area);
+        
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+        
+    }
+    
+    return answer;
+};
