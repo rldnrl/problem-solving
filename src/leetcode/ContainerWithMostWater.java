532713a49eae9c0a35390dc5ea494c82ded5f766
rldnrl <devpark1002@gmail.com>
2023-01-03 20:43:21 +0900

    feat: implement container with most water with optimization

--- src/leetcode/ContainerWithMostWater.java
+++ src/leetcode/ContainerWithMostWater.java
@@ -17,8 +17,32 @@ public class ContainerWithMostWater {
         return answer;
     }
 
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static int maxAreaWithOptimized(int[] height) {
+        int answer = 0;
+
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            answer = Math.max(answer, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] height = new int[]{1,8,6,2,5,4,8,3,7};
         System.out.println(maxAreaWithBruteforce(height));
+        System.out.println(maxAreaWithOptimized(height));
     }
 }
