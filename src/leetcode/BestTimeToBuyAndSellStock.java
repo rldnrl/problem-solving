64b18e2d555c386e568ba7805c016e5aa0b9d2f3
rldnrl <devpark1002@gmail.com>
2023-01-10 20:50:55 +0900

    feat: implement best time to buy and sell stock

---
+++ src/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+public class BestTimeToBuyAndSellStock {
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static int maxProfitWithSlidingWindow(int[] prices) {
+        int maxProfit = 0;
+        int left = 0;
+        int right = 1;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
