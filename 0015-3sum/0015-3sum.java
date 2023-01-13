3360db78784adab0087d78c1c4a0140fc9a3e485
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-13 21:59:44 +0900

    Time: 35 ms (54.91%), Space: 50.3 MB (50.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> answer = new LinkedList<>();
         
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
