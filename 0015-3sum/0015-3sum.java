f1acd11effa8b60517cbfb027055279ca8ef361f
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-13 21:59:23 +0900

    Time: 40 ms (46.85%), Space: 50.6 MB (50.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answer = new LinkedList<>();
+        
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
             int left = i + 1;
             int right = nums.length - 1;
-
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
+                if (sum < 0) {
                     left++;
+                } else if (sum > 0) {
+                    right--;
                 } else {
                     List<Integer> list = new LinkedList<>();
                     list.add(nums[i]);
@@ -30,7 +28,7 @@ class Solution {
                 }
             }
         }
-
+        
         return answer;
     }
 }
