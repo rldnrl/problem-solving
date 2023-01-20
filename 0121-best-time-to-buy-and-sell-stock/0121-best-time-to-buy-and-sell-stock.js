8f731d0dc60910e2f5daac704bffed94ea83fc3b
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-20 16:43:16 +0900

    Time: 90 ms (76.92%), Space: 51.1 MB (96.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    let answer = 0;
     let minPrice = Number.MAX_VALUE;
-    let maxProfit = 0;
     
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] < minPrice) {
             minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfit) {
-            maxProfit = prices[i] - minPrice;
+        } else if (prices[i] - minPrice > answer) {
+            answer = prices[i] - minPrice;
         }
     }
     
-    return maxProfit;
+    return answer;
 };
