45456653c8594dd0b5ed0856c095b805f257325f
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-24 18:08:42 +0900

    Time: 85 ms (65.16%), Space: 49.4 MB (56.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -4,13 +4,14 @@
  */
 var maxArea = function(height) {
     let answer = 0;
+    
     let left = 0;
     let right = height.length - 1;
     
     while (left < right) {
         const width = right - left;
         const area = width * Math.min(height[left], height[right]);
-        answer = Math.max(answer, area)
+        answer = Math.max(answer, area);
         
         if (height[left] < height[right]) {
             left++;
