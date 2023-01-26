62e6b9785b9b6033a593718f5d5e12bca924bb6b
Kiwook, Park <rldnrl1002@gmail.com>
2023-01-26 22:09:05 +0900

    Time: 34 ms (44.47%), Space: 48.9 MB (18.82%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() == 0) return "";
+        
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c: t.toCharArray()) {
+            int count = countT.getOrDefault(c, 0);
+            countT.put(c, count + 1);
+        }
+        
+        int[] res = new int[]{-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        
+        int have = 0;
+        int need = countT.size();
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char r = s.charAt(right);
+            
+            int countOfRight = window.getOrDefault(r, 0);
+            window.put(r, countOfRight + 1);
+            
+            if (countT.containsKey(r) && Objects.equals(window.get(r), countT.get(r))) {
+                have++;
+            }
+            
+            while (have == need) {
+                char l = s.charAt(left);
+                if (right - left + 1 < resLen) {
+                    res[0] = left;
+                    res[1] = right;
+                    resLen = right - left + 1;
+                }
+                int countOfLeft = window.get(l);
+                window.put(l, countOfLeft - 1);
+                
+                if (countT.containsKey(l) && window.get(l) < countT.get(l)) {
+                    have--;
+                }
+                left++;
+            }
+        }
+        
+        if (Objects.equals(resLen, Integer.MAX_VALUE)) return "";
+        
+        return s.substring(res[0], res[1] + 1);
+    }
+}
