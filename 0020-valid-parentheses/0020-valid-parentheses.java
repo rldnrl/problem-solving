1c725d71594744f6e7bff2d35082ec7448c21992
Kiwook, Park <rldnrl1002@gmail.com>
2023-02-09 11:32:40 +0900

    Time: 2 ms (60.37%), Space: 40.3 MB (60.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> hashMap = new HashMap<>();
-
-        hashMap.put(')', '(');
-        hashMap.put('}', '{');
-        hashMap.put(']', '[');
-
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put(')', '(');
+        brackets.put('}', '{');
+        brackets.put(']', '[');
+        
         Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (!hashMap.containsKey(s.charAt(i))) {
-                stack.push(s.charAt(i));
-            } else if (stack.isEmpty() || !Objects.equals(hashMap.get(s.charAt(i)), stack.pop())) {
+        for (char c: s.toCharArray()) {
+            if (!brackets.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.isEmpty() || brackets.get(c) != stack.pop()) {
                 return false;
             }
         }
-
+        
         return stack.isEmpty();
     }
 }
