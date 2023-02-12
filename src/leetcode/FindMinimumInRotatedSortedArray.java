d0204ca6523e927a89d10808ecb6a72e8bbafe1d
rldnrl <devpark1002@gmail.com>
2023-02-12 17:02:17 +0900

    feat: implement FindMinimumInRotatedSortedArray

---
+++ src/leetcode/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class FindMinimumInRotatedSortedArray {
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public int findMin(int[] nums) {
+        int answer = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (nums[left] < nums[right]) {
+                answer = Math.min(answer, nums[left]);
+                break;
+            }
+
+            int mid = (left + right) / 2;
+            answer = Math.min(answer, nums[mid]);
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
