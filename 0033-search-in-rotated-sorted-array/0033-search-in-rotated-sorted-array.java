618c6f00a798a3daae85e4e1df7418db5442dec6
Kiwook, Park <rldnrl1002@gmail.com>
2023-03-23 21:27:36 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (43.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,19 +6,19 @@ class Solution {
         while (left <= right) {
             int middle = (left + right) / 2;
             
-            if (nums[middle] == target) {
+            if (target == nums[middle]) {
                 return middle;
             }
             
             if (nums[left] <= nums[middle]) {
-                if (target > nums[middle] || target < nums[left]) {
+                if (target < nums[left] || target > nums[middle]) {
                     left = middle + 1;
                 } else {
-                    right = middle;
+                    right = middle - 1;
                 }
             } else {
-                if (target < nums[middle] || target > nums[right]) {
-                    right = middle;
+                if (target > nums[right] || target < nums[middle]) {
+                    right = middle - 1;
                 } else {
                     left = middle + 1;
                 }
