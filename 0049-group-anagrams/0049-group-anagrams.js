a7be1c3ad3adfa6b55d68fa0feb61b62d7880fe2
Kiwook, Park <rldnrl1002@gmail.com>
2023-04-08 23:33:18 +0900

    Time: 122 ms (77.23%), Space: 54.3 MB (25.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,18 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const map = strs.reduce((prev, cur) => {
-        const frequency = Array.from({ length: 26 }).fill(0)
-        for (const c of cur) {
-            frequency[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
-        }
-        const key = frequency.map((count) => `#${count}`).join("")
-        const values = prev.get(key) || []
-        values.push(cur)
-        prev.set(key, values)
-        
-        return prev
-    }, new Map())
+    const hashMap = new Map();
 
-    return [...map.values()]
+    for (const str of strs) {
+      const sortedStr = str.split("").sort().join("");
+      const arrayInHashMap = hashMap.get(sortedStr) || [];
+      arrayInHashMap.push(str);
+      hashMap.set(sortedStr, arrayInHashMap)
+    }
+
+    return [...hashMap.values()];
 };
