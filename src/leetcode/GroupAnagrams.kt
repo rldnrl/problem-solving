46ebfa19666f8531e36261bed58a2e841e0987ab
rldnrl <devpark1002@gmail.com>
2023-07-22 00:12:06 +0900

    feat: implement group anagram with kotlin

---
+++ src/leetcode/GroupAnagrams.kt
@@ -0,0 +1,39 @@
+package leetcode
+
+class GroupAnagrams {
+    fun solutionWithSorting(strs: Array<String>): List<List<String>> {
+        val result = mutableMapOf<String, MutableList<String>>()
+
+        for (str in strs) {
+            val key = str.toCharArray().sorted().joinToString("")
+
+            if (!result.containsKey(key)) {
+                result[key] = mutableListOf()
+            }
+
+            result[key]?.add(str)
+        }
+
+        return result.values.toList()
+    }
+
+    fun solutionWithArray(strs: Array<String>): List<List<String>> {
+        val alphabetSize = 26;
+        val result: MutableMap<String, MutableList<String>> = mutableMapOf();
+
+        for (str in strs) {
+            val charCount = IntArray(alphabetSize);
+            for (c in str) {
+                charCount[c - 'a']++;
+            }
+            val key = charCount.joinToString(",");
+
+            if (!result.containsKey(key)) {
+                result[key] = mutableListOf()
+            }
+            result[key]?.add(str)
+        }
+
+        return result.values.toList();
+    }
+}
