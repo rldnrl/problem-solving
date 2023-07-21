02f5e50df36ea895aaaa9825f8434a1653830739
Kiwook, Park <rldnrl1002@gmail.com>
2023-07-21 23:49:07 +0900

    Time: 419 ms (37.80%), Space: 43.7 MB (63.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.kt
+++ 0049-group-anagrams/0049-group-anagrams.kt
@@ -1,21 +1,17 @@
 class Solution {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        val alphabetSize = 26;
-        val result: MutableMap<String, MutableList<String>> = mutableMapOf();
+        val result = mutableMapOf<String, MutableList<String>>()
 
         for (str in strs) {
-            val charCount = IntArray(alphabetSize);
-            for (c in str) {
-                charCount[c - 'a']++;
-            }
-            val key = charCount.joinToString(",");
+            val key = str.toCharArray().sorted().joinToString("")
 
             if (!result.containsKey(key)) {
                 result[key] = mutableListOf()
             }
+
             result[key]?.add(str)
         }
 
-        return result.values.toList();
+        return result.values.toList()
     }
 }
