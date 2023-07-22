14e5238eb4ef3070c200b6e87a825b1166bb8a34
rldnrl <devpark1002@gmail.com>
2023-07-22 16:21:58 +0900

    feat: implement top k frequent with bucket sort

---
+++ src/leetcode/kotlin/TopKFrequent.kt
@@ -0,0 +1,58 @@
+package leetcode.kotlin
+
+import java.util.LinkedList
+
+class TopKFrequent {
+    fun solutionWithSort(nums: IntArray, k: Int): IntArray {
+        val result = IntArray(k)
+        val map = mutableMapOf<Int, Int>()
+
+        for (num in nums) {
+            map[num] = map.getOrDefault(num, 0) + 1
+        }
+
+        val entries = LinkedList(map.entries)
+        entries.sortBy { -it.value }
+
+        var i = 0;
+        for (entry in entries) {
+            result[i++] = entry.key
+            if (i == k) {
+                return result
+            }
+        }
+
+        return result
+    }
+    fun solutionWithBucketSort(nums: IntArray, k: Int): IntArray {
+        val answer = IntArray(k);
+        val bucket: MutableMap<Int, MutableList<Int>> = mutableMapOf()
+        val freq: MutableMap<Int, Int> = mutableMapOf()
+
+        for (i in 0..nums.size) {
+            bucket.putIfAbsent(i, mutableListOf())
+        }
+
+        for (num in nums) {
+            freq[num] = freq.getOrDefault(num, 0) + 1
+        }
+
+        for (entries in freq.entries) {
+            val num = entries.key
+            val count = entries.value
+            bucket[count]?.add(num)
+        }
+
+        var i = 0
+        for (num in nums.size downTo 0) {
+            for (element in bucket[num]!!) {
+                answer[i++] = element
+                if (i == k) {
+                    return answer
+                }
+            }
+        }
+
+        return answer
+    }
+}
