ba1b8dfa496c50f43e6f21471facb397e4d1125a
rldnrl <devpark1002@gmail.com>
2023-07-22 22:24:06 +0900

    refactor: refactor using toList function

--- src/leetcode/kotlin/TopKFrequent.kt
+++ src/leetcode/kotlin/TopKFrequent.kt
@@ -11,11 +11,10 @@ class TopKFrequent {
             map[num] = map.getOrDefault(num, 0) + 1
         }
 
-        val entries = LinkedList(map.entries)
-        entries.sortBy { -it.value }
+        val sortedEntries = map.entries.toList().sortedBy { -it.value }
 
         var i = 0;
-        for (entry in entries) {
+        for (entry in sortedEntries) {
             result[i++] = entry.key
             if (i == k) {
                 return result
