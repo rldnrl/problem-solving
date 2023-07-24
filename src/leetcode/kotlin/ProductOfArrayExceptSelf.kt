8711ddc10504acf4b5a8964de0cf8572d8a9d8e5
rldnrl <devpark1002@gmail.com>
2023-07-24 22:32:04 +0900

    feat: implement product of array except self

---
+++ src/leetcode/kotlin/ProductOfArrayExceptSelf.kt
@@ -0,0 +1,28 @@
+package leetcode.kotlin
+
+class ProductOfArrayExceptSelf {
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    fun solution1(nums: IntArray): IntArray {
+        val leftArray = IntArray(nums.size)
+        leftArray[0] = 1
+        for (i in 1..<nums.size) {
+            leftArray[i] = leftArray[i - 1] * nums[i - 1]
+        }
+
+        val rightArray = IntArray(nums.size)
+        rightArray[nums.size - 1] = 1
+        for (i in nums.size - 2 downTo 0) {
+            rightArray[i] = rightArray[i + 1] * nums[i + 1]
+        }
+
+        val answer = IntArray(nums.size)
+        for (i in nums.indices) {
+            answer[i] = leftArray[i] * rightArray[i]
+        }
+
+        return answer
+    }
+}
