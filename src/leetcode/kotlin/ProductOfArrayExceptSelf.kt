0953823e8fbe6151a0fb92369097817fa48098f6
rldnrl <devpark1002@gmail.com>
2023-07-29 15:20:41 +0900

    feat: implement optimized solution

--- src/leetcode/kotlin/ProductOfArrayExceptSelf.kt
+++ src/leetcode/kotlin/ProductOfArrayExceptSelf.kt
@@ -25,4 +25,24 @@ class ProductOfArrayExceptSelf {
 
         return answer
     }
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    fun solution2(nums: IntArray): IntArray {
+        val answer = IntArray(nums.size)
+        answer[0] = 1
+        for (i in 1..<nums.size) {
+            answer[i] = answer[i - 1] * nums[i - 1]
+        }
+
+        var right = 1
+        for (i in nums.size - 1 downTo 0) {
+            answer[i] *= right
+            right *= nums[i]
+        }
+
+        return answer
+    }
 }
