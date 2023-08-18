3b51b4ee9f9bcfc2272e947c7c98cc804d6f452e
rldnrl <devpark1002@gmail.com>
2023-08-18 21:01:04 +0900

    feat: linked list 역순으로 배열하기

---
+++ src/leetcode/java/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package leetcode.java;
+
+public class ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prevNode = null;
+        ListNode currNode = head;
+
+        while (currNode != null) {
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        return prevNode;
+    }
+}
