88fe5602106ea45821601e0ebf50ebd8dd9626a6
rldnrl <devpark1002@gmail.com>
2023-08-19 15:20:50 +0900

    feat: 링크드리스트 역순으로 배열하기

---
+++ src/leetcode/js/reverse-linked-list.js
@@ -0,0 +1,36 @@
+c/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseListWithIterative = function(head) {
+    let prevNode = null
+    let currNode = head
+
+    while (currNode !== null) {
+        const nextNode = currNode.next
+        currNode.next = prevNode
+        prevNode = currNode
+        currNode = nextNode
+    }
+
+    return prevNode
+};
+
+var reverseListWithRecursive = function(head) {
+    if (head === null || head.next === null) {
+        return head
+    }
+
+    const reversedSublist = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+
+    return reversedSublist
+};
