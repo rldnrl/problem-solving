63d9e7fdffa8dac128575823583c021362a8751c
rldnrl <devpark1002@gmail.com>
2023-10-08 00:22:41 +0900

    feat: 삼각 달팽이 두번째 풀이 테스트 코드 추가

--- test/programmers/lv2/TriangleSnailTest.java
+++ test/programmers/lv2/TriangleSnailTest.java
@@ -19,4 +19,19 @@ public class TriangleSnailTest {
         int[] result3 = new int[]{1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11};
         Assertions.assertArrayEquals(TriangleSnail.solution(input3), result3);
     }
+
+    @Test
+    void solution2Test() {
+        int input1 = 4;
+        int[] result1 = new int[]{1,2,9,3,10,8,4,5,6,7};
+        Assertions.assertArrayEquals(TriangleSnail.solution2(input1), result1);
+
+        int input2 = 5;
+        int[] result2 = new int[]{1,2,12,3,13,11,4,14,15,10,5,6,7,8,9};
+        Assertions.assertArrayEquals(TriangleSnail.solution2(input2), result2);
+
+        int input3 = 6;
+        int[] result3 = new int[]{1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11};
+        Assertions.assertArrayEquals(TriangleSnail.solution2(input3), result3);
+    }
 }
