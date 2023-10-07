59b0c92813b341af64429ecad705362c70c20fe6
rldnrl <devpark1002@gmail.com>
2023-10-08 00:22:46 +0900

    feat: 삼각 달팽이 두번째 풀이

--- src/programmers/lv2/TriangleSnail.java
+++ src/programmers/lv2/TriangleSnail.java
@@ -50,4 +50,40 @@ public class TriangleSnail {
 
         return answer;
     }
+
+    static final int[] dx = new int[]{0, 1, -1};
+    static final int[] dy = new int[]{1, 0, -1};
+
+    public static int[] solution2(int n) {
+        int[][] triangle = new int[n][n];
+        int x = 0;
+        int y = 0;
+        int d = 0;
+        int v = 1;
+
+        while (true) {
+            triangle[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+            }
+            x = nx;
+            y = ny;
+        }
+
+        int[] answer = new int[v - 1];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer[index++] = triangle[i][j];
+            }
+        }
+
+        return answer;
+    }
 }
