1e33937706509bd3ce3fdc3576df9a3e613675e9
rldnrl <devpark1002@gmail.com>
2023-10-09 22:25:38 +0900

    test: 거리두기 확인하기 테스트 코드

---
+++ test/programmers/lv2/CheckDistanceTest.java
@@ -0,0 +1,17 @@
+package programmers.lv2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class CheckDistanceTest {
+    @Test
+    void solutionTest() {
+        String[][] input1 = {{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
+        int[] answer = {1, 0, 1, 1, 1};
+        CheckDistance checkDistance = new CheckDistance();
+        System.out.println(Arrays.toString(checkDistance.solution(input1)));
+        Assertions.assertArrayEquals(checkDistance.solution(input1), answer);
+    }
+}
