0f6a5a72cf62631f280dff281a38133ec369b345
rldnrl <devpark1002@gmail.com>
2023-10-14 21:20:19 +0900

    feat: 행렬 곱

---
+++ src/programmers/lv2/MatrixMultiply.java
@@ -0,0 +1,22 @@
+package programmers.lv2;
+
+public class MatrixMultiply {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int row1 = arr1.length;
+        int col1 = arr1[0].length;
+        int col2 = arr2[0].length;
+        int[][] answer = new int[row1][col2];
+
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < col2; j++) {
+                int sum = 0;
+                for (int k = 0; k < col1; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = sum;
+            }
+        }
+
+        return answer;
+    }
+}
