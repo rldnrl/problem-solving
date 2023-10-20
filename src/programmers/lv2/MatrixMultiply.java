a4ae6109b99d7da06b9c7cc452b124ea82545480
rldnrl <devpark1002@gmail.com>
2023-10-20 18:46:38 +0900

    feat: 새로운 솔루션 추가

--- src/programmers/lv2/MatrixMultiply.java
+++ src/programmers/lv2/MatrixMultiply.java
@@ -19,4 +19,18 @@ public class MatrixMultiply {
 
         return answer;
     }
+
+    public int[][] solution2(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++) {
+                for (int k = 0; k <  arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
 }
