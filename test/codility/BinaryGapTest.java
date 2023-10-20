be9993f5a12c4fd2ca8954e220bb7650b989f2b7
rldnrl <devpark1002@gmail.com>
2023-10-20 18:46:57 +0900

    test: binary gap

---
+++ test/codility/BinaryGapTest.java
@@ -0,0 +1,11 @@
+package codility;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class BinaryGapTest {
+    @Test
+    void binaryGapTest() {
+        Assertions.assertEquals(BinaryGap.solution(9), 2);
+    }
+}
