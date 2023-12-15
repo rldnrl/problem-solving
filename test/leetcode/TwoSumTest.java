66f27da56222ee1c7a4e3edbc55807aa24a4da3e
rldnrl <devpark1002@gmail.com>
2023-12-15 23:24:46 +0900

    test: twoSum 세번째 풀이 테스트 코드

--- test/leetcode/TwoSumTest.java
+++ test/leetcode/TwoSumTest.java
@@ -41,4 +41,22 @@ class TwoSumTest {
         int[] result3 = new int[] {0, 1};
         Assertions.assertArrayEquals(TwoSum.twoSum2(testNum3, target3), result3);
     }
+
+    @Test
+    void twoSum3Test() {
+        int[] testNums1 = new int[] {2,7,11,15};
+        int target1 = 9;
+        int[] result1 = new int[] {0, 1};
+        Assertions.assertArrayEquals(TwoSum.twoSum3(testNums1, target1), result1);
+
+        int[] testNum2 = new int[] {3, 2, 4};
+        int target2 = 6;
+        int[] result2 =  new int[] {1, 2};
+        Assertions.assertArrayEquals(TwoSum.twoSum3(testNum2, target2), result2);
+
+        int[] testNum3 = new int[] {3, 3};
+        int target3 = 6;
+        int[] result3 = new int[] {0, 1};
+        Assertions.assertArrayEquals(TwoSum.twoSum3(testNum3, target3), result3);
+    }
 }
