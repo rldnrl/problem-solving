55ccb2016920860be1c07a841a2e90e924da128e
Kiwook, Park <rldnrl1002@gmail.com>
2023-12-31 17:50:42 +0900

    Time: 5 ms (48.96%), Space: 42.6 MB (62.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        Deque<Character> strs = new ArrayDeque<>();
+        int start = 0;
+        int end = s.length() - 1;
         
-        for (char c: s.toCharArray()) {
-            if (Character.isAlphabetic(c) || Character.isDigit(c)) {
-                strs.add(Character.toLowerCase(c));
-            }
-        }
-        
-        while (strs.size() > 1) {
-            if (strs.pollFirst() != strs.pollLast()) {
-                return false;
+        while (start < end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                
+                start++;
+                end--;
             }
         }
         
