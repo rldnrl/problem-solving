4315b4ac9e356ebef0112fe46f18189283dc40e3
Kiwook, Park <rldnrl1002@gmail.com>
2024-03-04 21:19:01 +0900

    Time: 74 ms (51.76%), Space: 54.4 MB (16.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,25 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    
+    if (s.length !== t.length) {
+        return false
+    }
+
     const LENGTH = 26
-       
-    const arrayOfS = Array(LENGTH).fill(0)
-    for (let i = 0; i < s.length; i++) {
-        arrayOfS[s[i].charCodeAt() - 'a'.charCodeAt()]++
+
+    const arrayS = Array(LENGTH).fill(0)
+    const arrayT = Array(LENGTH).fill(0)
+
+    for (const char of s) {
+        arrayS[char.charCodeAt() - 'a'.charCodeAt()]++
     }
-    
-    const arrayOfT = Array(LENGTH).fill(0)
-    for (let i = 0; i < t.length; i++) {
-        arrayOfT[t[i].charCodeAt() - 'a'.charCodeAt()]++
+
+    for (const char of t) {
+        arrayT[char.charCodeAt() - 'a'.charCodeAt()]++
     }
-       
+
     for (let i = 0; i < LENGTH; i++) {
-        if (arrayOfS[i] !== arrayOfT[i]) {
-            return false
+        if (arrayS[i] !== arrayT[i]) {
+          return false
         }
     }
-    
+
     return true
 };
