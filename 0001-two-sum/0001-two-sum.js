6b848c5f5ae527744044b180ee0136712303776b
Kiwook, Park <rldnrl1002@gmail.com>
2024-03-04 21:38:36 +0900

    Time: 48 ms (96.44%), Space: 51.9 MB (10.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const map = new Map()
+    
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-        const complement = target - num
-        
-        if (nums.slice(i + 1).includes(complement)) {
-            return [nums.indexOf(num), nums.slice(i + 1).indexOf(complement) + (i + 1)]
+        if (map.has(target - nums[i])) {
+            return [map.get(target-nums[i]), i]
         }
+        
+        map.set(nums[i], i)
     }
+    
+    return []
 };
