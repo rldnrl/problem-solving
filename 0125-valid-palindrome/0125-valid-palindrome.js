e8f94300aaf55e9010de9720a5e0444a19e3a24a
Kiwook, Park <rldnrl1002@gmail.com>
2024-03-11 21:07:18 +0900

    Time: 288 ms (5.03%), Space: 61.3 MB (5.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,31 +3,28 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const LENGTH = 26
+  const LENGTH = 26
 
-    const isAlphabet = (char) => {
-      const array = []
-      for (let i = 97; i < 97 + LENGTH; i++) {
-        array.push(i)
-      }
-
-      for (let i = 65; i < 65 + LENGTH; i++) {
-        array.push(i)
-      }
-
-      return array.includes(char.charCodeAt())
+  const isAlphabet = (char) => {
+    const array = []
+    for (let i = 97; i < 97 + LENGTH; i++) {
+      array.push(i)
     }
 
-    s = s.toLocaleLowerCase().split("").filter(char => isAlphabet(char) || !isNaN(char)).filter((char) => char !== " ").join("")
-
-    let left = 0, right = s.length - 1
-    while (left <= right) {
-      if (s[left] !== s[right]) {
-        return false;
-      }
-      left++;
-      right--
+    for (let i = 65; i < 65 + LENGTH; i++) {
+      array.push(i)
     }
 
-    return true
+    return array.includes(char.charCodeAt())
+  }
+
+  const deque = s.toLocaleLowerCase().split("").filter(char => isAlphabet(char) || !isNaN(char)).filter((char) => char !== " ")
+
+  while (deque.length > 1) {
+    if (deque.shift() !== deque.pop()) {
+      return false;
+    }
+  }
+
+  return true
 };
