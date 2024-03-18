59111d0cdff3d70cfb2588931a778df609600a9a
Kiwook, Park <rldnrl1002@gmail.com>
2024-03-18 21:06:41 +0900

    Time: 93 ms (36.52%), Space: 64.5 MB (9.81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,19 +3,24 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const size = nums.length;
-    const answer = Array.from({ legnth: size });
+    const N = nums.length
     
-    answer[0] = 1
-    for (let i = 1; i < size; i++) {
-        answer[i] = answer[i - 1] * nums[i - 1];
+    const leftArray = Array(N).fill(1)
+    const rightArray = Array(N).fill(1)
+    
+    for (let i = 1; i < N; i++) {
+        leftArray[i] = leftArray[i - 1] * nums[i - 1]
     }
     
-    let right = 1;
-    for (let i = size - 1; i >= 0; i--) {
-        answer[i] = answer[i] * right;
-        right = right * nums[i];
+    for (let i = N - 2; i >= 0; i--) {
+        rightArray[i] = rightArray[i + 1] * nums[i + 1]
     }
     
-    return answer;
+    const answer = []
+    
+    for (let i = 0; i < N; i++) {
+        answer[i] = leftArray[i] * rightArray[i]
+    }
+    
+    return answer
 };
