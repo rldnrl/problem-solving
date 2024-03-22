cda73f7a545944aa2dcb191ee813cf32ad95eca5
Kiwook, Park <rldnrl1002@gmail.com>
2024-03-22 21:17:54 +0900

    Time: 53 ms (79.48%), Space: 49.7 MB (75.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,16 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const map = new Map([['}', '{'], [')', '('], [']', '[']])
-
+    const map = new Map()
+    
+    map.set(')', '(')
+    map.set(']', '[')
+    map.set('}', '{')
+    
     const stack = []
+    
     for (const c of s) {
         if (!map.has(c)) {
-          stack.push(c)
+            stack.push(c)
         } else if (stack.length === 0 || map.get(c) !== stack.pop()) {
-          return false
+            return false
         }
     }
-
+    
     return stack.length === 0
 };
