5cac026cc5bb522dea84247398a53849bbb362db
Kiwook, Park <rldnrl1002@gmail.com>
2024-05-27 21:40:27 +0900

    Time: 0 ms (100.00%), Space: 45.5 MB (16.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,29 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        
-        if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        
-        else if (nums[0] != 0) {
-            return 0;
-        }
-        
-        int start = Arrays.stream(nums).min().getAsInt();
-        int end = Arrays.stream(nums).max().getAsInt();
-        
-        int answer = 0;
-        for (int i = start; i <= end; i++) {
-            answer += i;
-        }
-        
-        int numSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            answer -= nums[i];
-        }
-        
-        
-        return answer;
+        int expectedSum = nums.length * (nums.length + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) actualSum += num;
+        return expectedSum - actualSum;
     }
 }
